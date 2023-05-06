523abb68414088ccc10527babe0ba5fd0a5e00de
Adrian Rodil <adrianrodil1998@gmail.com>
2023-05-06 13:51:04 +0200

    store list of kickable users

--- src/services/DiscordService.js
+++ src/services/DiscordService.js
@@ -1,5 +1,15 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const {
+  Client,
+  GatewayIntentBits,
+  REST,
+  Routes,
+  SlashCommandBuilder,
+  SlashCommandUserOption,
+} = require("discord.js");
 const TOKEN = process.env.appToken;
+const CLIENT_ID = process.env.appId;
+
+const usersToKickIds = [];
 
 const client = new Client({
   intents: [GatewayIntentBits.GuildVoiceStates],
@@ -10,7 +20,49 @@ client.on("ready", () => {
 });
 
 client.on("voiceStateUpdate", (_, newState) => {
-  console.log(newState);
+  if (!newState.channelId) {
+    console.log("User is not in a channel. Skipping action");
+    return;
+  }
+  newState.member.voice.disconnect();
 });
-console.log("patata");
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "dkick") {
+    const data = interaction.options.data;
+    console.log("DKICK TRRIGGERED");
+    console.log(data);
+    const userId = data[0].user.id;
+    usersToKickIds.push(userId);
+    interaction.reply({
+      content: "Kick registered for user with id " + userId,
+      ephemeral: true,
+    });
+  }
+});
+
+// register slash commands
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("dkick")
+    .setDescription("Kick a user after they join")
+    .addUserOption(() =>
+      new SlashCommandUserOption()
+        .setName("dkick")
+        .setDescription("User to kick")
+        .setRequired(true)
+    ),
+];
+
+const rest = new REST({ version: "10" }).setToken(TOKEN);
+
+console.log("Started refreshing application (/) commands.");
+rest
+  .put(Routes.applicationCommands(CLIENT_ID), { body: commands })
+  .catch(console.error)
+  .then(() => console.log("Successfully reloaded application (/) commands."));
+
 client.login(TOKEN);
