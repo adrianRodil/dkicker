8cbf23c94c6a70c76287a7820e1d46c6d05b092b
Adrian Rodil <adrianrodil1998@gmail.com>
2023-05-06 14:33:32 +0200

    give delay range in slash commands

--- src/services/DiscordService.js
+++ src/services/DiscordService.js
@@ -5,11 +5,12 @@ const {
   Routes,
   SlashCommandBuilder,
   SlashCommandUserOption,
+  SlashCommandIntegerOption,
 } = require("discord.js");
 const TOKEN = process.env.appToken;
 const CLIENT_ID = process.env.appId;
 
-const usersToKickIds = [];
+const usersToKick = [];
 
 const client = new Client({
   intents: [GatewayIntentBits.GuildVoiceStates],
@@ -25,12 +26,16 @@ client.on("voiceStateUpdate", (_, newState) => {
     return;
   }
   const user = newState.member;
-  if (!usersToKickIds.includes(user.user.id)) {
+  const kickUserConfig = usersToKick.find(
+    (userConfig) => userConfig.id === user.user.id
+  );
+  if (!kickUserConfig) {
     console.log("user not in kick list skipping");
     return;
   }
 
-  const kickInSeconds = Math.floor(Math.random() * 15) + 5;
+  const kickInSeconds =
+    Math.floor(Math.random() * kickUserConfig.range) + kickUserConfig.delay;
   console.log(`Kicking user in ${kickInSeconds} seconds`);
   setTimeout(() => {
     console.log("Kicking user");
@@ -45,7 +50,10 @@ client.on("interactionCreate", async (interaction) => {
     const data = interaction.options.data;
     console.log("DKICK TRRIGGERED");
     const userId = data[0].user.id;
-    usersToKickIds.push(userId);
+    const delay = data[1].value;
+    const range = data[2].value;
+    usersToKick.push({ userId, delay, range });
+    console.log(usersToKick);
     interaction.reply({
       content: "Kick registered for user with id " + userId,
       ephemeral: true,
@@ -59,10 +67,21 @@ const commands = [
   new SlashCommandBuilder()
     .setName("dkick")
     .setDescription("Kick a user after they join")
-    .addUserOption(() =>
-      new SlashCommandUserOption()
-        .setName("dkick")
-        .setDescription("User to kick")
+    .addUserOption((option) =>
+      option.setName("user").setDescription("User to kick").setRequired(true)
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName("delay")
+        .setMinValue(0)
+        .setDescription("Delay start")
+        .setRequired(true)
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName("range")
+        .setMinValue(0)
+        .setDescription("Random interval")
         .setRequired(true)
     ),
 ];
